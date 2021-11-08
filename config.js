a5a1a52d8a679a7c3b738ddaf02908e08da5f47c
THE-UNKNOWN-CREATOR <70652146+THE-UNKNOWN-CREATOR@users.noreply.github.com>
2021-11-08 19:17:53 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAWaMIYzme0QVJa9QIknDlBEy8RsjJKZmQ",
+  authDomain: "storytellingapp-e87e9.firebaseapp.com",
+  projectId: "storytellingapp-e87e9",
+  storageBucket: "storytellingapp-e87e9.appspot.com",
+  messagingSenderId: "518312558867",
+  appId: "1:518312558867:web:d9d06504b0d424f33cdae6"
+};
